9247c8b9f21e0afbcadb82428c4fd794b973b556
AYUSHBHASKARSINGH <95188806+AYUSHBHASKARSINGH@users.noreply.github.com>
2024-07-27 23:03:18 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -20,7 +20,9 @@ dotenv.config({path: "./config/config.env"})
 
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
+    // origin: [process.env.FRONTEND_URL],
+    origin: ["job-zee-zq23-frontend.vercel.app/"],
+
     methods: ["GET","POST","DELETE","PUT"],
     credentials: true,
 }))
